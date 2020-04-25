f4e9083e45b76e4bcb3b0c9846659c8ade8db05e
QuocHoang94 <39055816+QuocHoang94@users.noreply.github.com>
2020-04-25 09:19:43 +0700

    Update configureStore.js

--- react-saga/src/redux/configureStore.js
+++ react-saga/src/redux/configureStore.js
@@ -1,16 +1,23 @@
 import { createStore,compose,applyMiddleware } from 'redux';
 import rootReducer from './../reducers/index';
+//tích hợp devtool
 const composeEnhancers = process.env.NODE_ENV !== 'production' &&
 typeof window === 'object' &&
 window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__?
 window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
     shouldHotReload: false
 }) : compose;
+//tích hợp devtool
+
+//tích hợp middleware
 const configureStore = () => {
     const middlewares = [];
     const enhancers = [
         applyMiddleware(...middlewares)
     ]
+//tích hợp middleware
+ 
+//create a sotre tích hợp devtool và middleware
     const store = createStore(rootReducer,composeEnhancers(...enhancers));
     return store;
 };
